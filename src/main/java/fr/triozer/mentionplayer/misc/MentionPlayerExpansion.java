ca77343dc7296758512034ebca984a38d9ce09b4
Cédric <cboirard@scortex.io>
2020-03-28 11:25:38 +0100

    lint: fix bad indentations

--- src/main/java/fr/triozer/mentionplayer/misc/MentionPlayerExpansion.java
+++ src/main/java/fr/triozer/mentionplayer/misc/MentionPlayerExpansion.java
@@ -54,10 +54,9 @@ public class MentionPlayerExpansion extends PlaceholderExpansion {
             }
         } else if (identifier.contains("state")) {
             boolean isMentionnable = MPlayer.get(player.getUniqueId()).allowMention();
-	    return isMentionnable ? ChatColor.RED + "yes" : ChatColor.GREEN + "no";
+            return isMentionnable ? ChatColor.RED + "yes" : ChatColor.GREEN + "no";
 	}
 
         return null;
-
     }
 }
